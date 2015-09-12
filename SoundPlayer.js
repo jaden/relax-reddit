165761bc7dadfa2062f2deb7eb6eb507b7a478cd
Dan Hersam <dan@hersam.com>
2015-09-11 21:13:36 -0400

    Indentation with spaces

--- SoundPlayer.js
+++ SoundPlayer.js
@@ -1,65 +1,65 @@
 function SoundPlayer() {
-	SC.initialize({
-  		client_id: 'd8c4984b4e6b9038bdf5f16703eed079'
-	});
+    SC.initialize({
+        client_id: 'd8c4984b4e6b9038bdf5f16703eed079'
+    });
 }
 
 SoundPlayer.prototype.playNewTrack = function(callback) {
 
-	var self = this;
+    var self = this;
 
-	SC.get('/tracks', { q: 'soothing', tags: 'instrumental', bpm: { to: 60 } }, function(tracks) {
-  		self.track = tracks[getRandomInt(tracks.length)];
+    SC.get('/tracks', { q: 'soothing', tags: 'instrumental', bpm: { to: 60 } }, function(tracks) {
+        self.track = tracks[getRandomInt(tracks.length)];
 
-  		SC.stream("/tracks/" + self.track.id, function(controls) {
-			self.setControls(controls);
-			self.play();
-		});
+        SC.stream("/tracks/" + self.track.id, function(controls) {
+            self.setControls(controls);
+            self.play();
+        });
 
-		callback(self.track);
-	});
+        callback(self.track);
+    });
 };
 
 SoundPlayer.prototype.isPlaying = function() {
-	if (this.controls && this.controls.getState() === 'playing') {
-		return true;
-	}
+    if (this.controls && this.controls.getState() === 'playing') {
+        return true;
+    }
 
-	return false;
+    return false;
 };
 
 SoundPlayer.prototype.playNewTrackIfEnded = function(callback) {
-	if (this.controls && this.controls.getState() === 'ended') {
-		this.playNewTrack(callback);
-	}
+    if (this.controls && this.controls.getState() === 'ended') {
+        this.playNewTrack(callback);
+    }
 };
 
 SoundPlayer.prototype.setControls = function(controls) {
 
-	if (this.controls) {
-		this.controls.stop();
-	}
+    if (this.controls) {
+        this.controls.stop();
+    }
 
-	this.controls = controls;
+    this.controls = controls;
 };
 
 SoundPlayer.prototype.play = function(callback) {
-	if (this.controls) {
-		this.controls.setVolume(0.2);
-		this.controls.play();
-	} else {
-		this.playNewTrack(callback);
-	}
+    if (this.controls) {
+        this.controls.setVolume(0.2);
+        this.controls.play();
+    } else {
+        this.playNewTrack(callback);
+    }
 };
 
 SoundPlayer.prototype.stop = function() {
-	if (this.controls) {
-		this.controls.stop();
-	}
+    if (this.controls) {
+        this.controls.stop();
+    }
 };
 
 SoundPlayer.prototype.pause = function() {
-	if (this.controls) {
-		this.controls.pause();
-	}
+    if (this.controls) {
+        this.controls.pause();
+    }
 };
--- app.js
+++ app.js
@@ -1,186 +1,186 @@
-var	BGS_COUNT    = 100,
+var BGS_COUNT    = 100,
     QUOTES_COUNT = 100;
 
 window.vue = new Vue({
-	el: '#container',
+    el: '#container',
 
-	data: {
-		backgrounds: [],
-		background: '',
-		backgroundImage: '',
-		cachedBackground: '',
-		cachedBackgroundImage: '',
-		quotes: [],
-		quote: '',
-		musicPlaying: false,
-		currentTrack: '',
-		soundPlayer: null,
-		refresh: null,
-		refreshInterval: 15000,
-	},
+    data: {
+        backgrounds: [],
+        background: '',
+        backgroundImage: '',
+        cachedBackground: '',
+        cachedBackgroundImage: '',
+        quotes: [],
+        quote: '',
+        musicPlaying: false,
+        currentTrack: '',
+        soundPlayer: null,
+        refresh: null,
+        refreshInterval: 15000,
+    },
 
-	created: function () {
+    created: function () {
 
-		this.loadBackgroundImages();
-		this.loadQuotes();
-		this.toggleRefresh();
-	},
+        this.loadBackgroundImages();
+        this.loadQuotes();
+        this.toggleRefresh();
+    },
 
-	methods: {
+    methods: {
 
-		loadBackgroundImages: function() {
+        loadBackgroundImages: function() {
 
-			$.getJSON("https://www.reddit.com/r/earthporn/top.json?sort=top&t=all&limit=" + BGS_COUNT, function(json) {
+            $.getJSON("https://www.reddit.com/r/earthporn/top.json?sort=top&t=all&limit=" + BGS_COUNT, function(json) {
 
-				this.backgrounds = json.data.children;
-				this.chooseBackgroundImage();
+                this.backgrounds = json.data.children;
+                this.chooseBackgroundImage();
 
-			}.bind(this));
-		},
+            }.bind(this));
+        },
 
-		chooseBackgroundImage: function() {
+        chooseBackgroundImage: function() {
 
-			this.cachedBackground      = this.getRandomBackground();
-			this.cachedBackgroundImage = imageUrl(this.cachedBackground.url);
+            this.cachedBackground      = this.getRandomBackground();
+            this.cachedBackgroundImage = imageUrl(this.cachedBackground.url);
 
-			// Handle first page load
-			if (! this.background) {
-				this.background = this.getRandomBackground();
-			} else {
-				this.background = this.cachedBackground;
-			}
+            // Handle first page load
+            if (! this.background) {
+                this.background = this.getRandomBackground();
+            } else {
+                this.background = this.cachedBackground;
+            }
 
-			this.backgroundImage = this.getBackgroundImageUrl();
-		},
+            this.backgroundImage = this.getBackgroundImageUrl();
+        },
 
-		getRandomBackground: function() {
-			return this.backgrounds[getRandomInt(BGS_COUNT)].data;
-		},
+        getRandomBackground: function() {
+            return this.backgrounds[getRandomInt(BGS_COUNT)].data;
+        },
 
-		getBackgroundImageUrl: function() {
-			var path = 'url('
-				+ imageUrl(this.background.url)
-				+ ')';
+        getBackgroundImageUrl: function() {
+            var path = 'url('
+                + imageUrl(this.background.url)
+                + ')';
 
-			return path;
-		},
+            return path;
+        },
 
-		loadQuotes: function() {
+        loadQuotes: function() {
 
-			$.getJSON("https://www.reddit.com/r/quotes/top.json?sort=top&t=all&limit=" + QUOTES_COUNT, function(json) {
+            $.getJSON("https://www.reddit.com/r/quotes/top.json?sort=top&t=all&limit=" + QUOTES_COUNT, function(json) {
 
-				this.quotes = json.data.children;
-				this.chooseQuote(false);
+                this.quotes = json.data.children;
+                this.chooseQuote(false);
 
-			}.bind(this));
-		},
+            }.bind(this));
+        },
 
-		chooseQuote: function(fade) {
+        chooseQuote: function(fade) {
 
-			if (! fade) {
-				this.quote = this.quotes[getRandomInt(QUOTES_COUNT)].data;
-				return;
-			}
+            if (! fade) {
+                this.quote = this.quotes[getRandomInt(QUOTES_COUNT)].data;
+                return;
+            }
 
-			// TODO Use v-transition instead of jQuery http://vuejs.org/guide/transitions.html#CSS_Transitions
-			$('#quote').fadeOut(2000, function() {
-				this.quote = this.quotes[getRandomInt(QUOTES_COUNT)].data;
-				$('#quote').fadeIn(2000);
-			}.bind(this));
-		},
+            // TODO Use v-transition instead of jQuery http://vuejs.org/guide/transitions.html#CSS_Transitions
+            $('#quote').fadeOut(2000, function() {
+                this.quote = this.quotes[getRandomInt(QUOTES_COUNT)].data;
+                $('#quote').fadeIn(2000);
+            }.bind(this));
+        },
 
-		togglePlaying: function() {
+        togglePlaying: function() {
 
-			if (! this.soundPlayer) {
-				this.soundPlayer = new SoundPlayer();
-			}
+            if (! this.soundPlayer) {
+                this.soundPlayer = new SoundPlayer();
+            }
 
-			if (this.soundPlayer.isPlaying())
-			{
-				this.soundPlayer.pause();
-				this.musicPlaying = false;
-			}
-			else {
-				this.soundPlayer.play(this.setTrack);
-				this.musicPlaying = true;
-			}
-		},
+            if (this.soundPlayer.isPlaying())
+            {
+                this.soundPlayer.pause();
+                this.musicPlaying = false;
+            }
+            else {
+                this.soundPlayer.play(this.setTrack);
+                this.musicPlaying = true;
+            }
+        },
 
-		toggleRefresh: function() {
-			if (this.refresh) {
-				clearTimeout(this.refresh);
-				this.refresh = null;
-				return;
-			}
+        toggleRefresh: function() {
+            if (this.refresh) {
+                clearTimeout(this.refresh);
+                this.refresh = null;
+                return;
+            }
 
-			this.refresh = setInterval(function() {
+            this.refresh = setInterval(function() {
 
-				this.chooseBackgroundImage();
-				this.chooseQuote(true);
+                this.chooseBackgroundImage();
+                this.chooseQuote(true);
 
-				if (this.musicPlaying) {
-					this.soundPlayer.playNewTrackIfEnded(this.setTrack);
-				}
+                if (this.musicPlaying) {
+                    this.soundPlayer.playNewTrackIfEnded(this.setTrack);
+                }
 
-			}.bind(this), this.refreshInterval);
-		},
+            }.bind(this), this.refreshInterval);
+        },
 
-		newTrack: function() {
-			this.musicPlaying = true;
-			this.soundPlayer.playNewTrack(this.setTrack);
-		},
+        newTrack: function() {
+            this.musicPlaying = true;
+            this.soundPlayer.playNewTrack(this.setTrack);
+        },
 
-		// Callback for playNewTrack or play
-		setTrack: function(track) {
-			this.currentTrack = track;
-		},
-	},
+        // Callback for playNewTrack or play
+        setTrack: function(track) {
+            this.currentTrack = track;
+        },
+    },
 
-	computed: {
-		playOrPauseText: function() {
-			return (this.musicPlaying)
-				? 'Pause'
-				: 'Play';
-		},
+    computed: {
+        playOrPauseText: function() {
+            return (this.musicPlaying)
+                ? 'Pause'
+                : 'Play';
+        },
 
-		stopRefreshingText: function() {
-			return (this.refresh)
-				? 'Stop'
-				: 'Resume';
-		}
-	},
+        stopRefreshingText: function() {
+            return (this.refresh)
+                ? 'Stop'
+                : 'Resume';
+        }
+    },
 });
 
 // Get an actual image url rather than link to imgur page
 function imageUrl(url) {
-	if (! url) {
-		return url;
-	}
+    if (! url) {
+        return url;
+    }
 
-	var ext = getExtension(url);
+    var ext = getExtension(url);
 
-	if (ext === '') {
-		return url + '.jpg';
-	}
+    if (ext === '') {
+        return url + '.jpg';
+    }
 
-	if (ext === 'gifv') {
-		return url + 'gif';
-	}
+    if (ext === 'gifv') {
+        return url + 'gif';
+    }
 
-	return url;
+    return url;
 }
 
 function getExtension(url) {
-	var lastPathSegment = url.substr(url.lastIndexOf('/') + 1);
-	var extension = lastPathSegment.substr(lastPathSegment.lastIndexOf('.') + 1);
+    var lastPathSegment = url.substr(url.lastIndexOf('/') + 1);
+    var extension = lastPathSegment.substr(lastPathSegment.lastIndexOf('.') + 1);
 
-	if (lastPathSegment === extension) {
-		return '';
-	}
+    if (lastPathSegment === extension) {
+        return '';
+    }
 
-	return extension;
+    return extension;
 }
 
 function getRandomInt(max) {
-	return Math.floor(Math.random() * max);
+    return Math.floor(Math.random() * max);
 }
